2532d0463485f31e08ff9b86a6fb75cdc98cf014
fitomega <76403260+fitomega@users.noreply.github.com>
2025-08-10 21:20:19 +0200

    Neurones connection generation via searchig for other neurones that are active working

--- objects/neuronal_network.gd
+++ objects/neuronal_network.gd
@@ -1,1 +1,41 @@
-extends Node
+extends Node2D
+class_name NeuronalNetwork
+
+var neurones = Array([], TYPE_OBJECT, "Node", Neurone)
+var connections = []
+
+var active_neurones
+
+#test
+var process_time = 0
+
+func _ready() -> void:
+	# Rellenamos las neuronas de forma random
+	for i in range(1, 5): neurones.append(Neurone.InputN.new("N" + str(i) + "| Input", 0, 0))
+	for i in range(5, 10): neurones.append(Neurone.OutputN.new("N" + str(i) + "| Output", 0, 0))
+	for i in range(10, 15): neurones.append(Neurone.MiddleN.new("N" + str(i) + "| Middle", 0, 0))
+	
+	neurones.any(func(n:Neurone): 
+		self.add_child(n)
+		n.position.x = randi_range(20, 500)
+		n.position.y = randi_range(20, 500)
+	)
+	
+func _process(delta: float) -> void:
+	process_time += 1
+	active_neurones = neurones.filter(func(n): return n.state == true)
+	
+	# active_neurones.any(func(n : Neurone): print(n is Neurone.OutputN))
+	
+	for n1 : Neurone in active_neurones:
+		active_neurones.any(
+			func(n2): 
+				if(n1 == n2): return false
+				elif(n1 is Neurone.OutputN): return false
+				elif(n1 is Neurone.MiddleN && n2 is Neurone.InputN): return false
+				elif n1.connections.filter(func(c : Connection): return c.target == n2 || c.origin == n2).size() == 0:
+					print(n1.id, "->", n2.id, " delta: " + str(process_time))
+					var new_conn = Connection.new(n1.id + " " + n2.id, n1, n2)
+					connections.append(new_conn)
+					self.add_child(new_conn)
+		)
--- objects/neurone.gd
+++ objects/neurone.gd
@@ -1,1 +1,67 @@
-extends Node
+extends Node2D
+class_name Neurone
+
+var id : String
+var value : float: 
+	set(new_value): if(new_value <= 1 && new_value >= 0): value = new_value
+	else: printerr("Value too large or too small")
+var threshold : float
+var timer : Timer
+var recieved_value : float
+var connections : Array
+var state : bool
+
+signal fired
+
+var color : Color = Color.WHITE
+#Only for testing
+func _draw() -> void: draw_circle(Vector2(0,0),5,color,true)
+func _init(id, value, threshold = 0) -> void:
+	self.id = id
+	self.value = value
+	self.threshold = threshold
+	self.state = false
+	self.name = id
+func _ready() -> void:
+	timer = Timer.new()
+	self.add_child(timer)
+
+func fire(): 
+	self.state = true
+	for c in connections:
+		c.send(self, self.value)
+	self.color = Color.RED
+	queue_redraw()
+	
+
+func recieve(new_value):
+	self.recieved_value += new_value 
+	if self.recieved_value >= threshold:
+		self.fire()
+	timer.start(1)
+	timer.timeout.connect(func():
+		self.state = false
+		self.recieved_value = 0
+		self.color = Color.WHITE
+		queue_redraw()
+	)
+
+class OutputN: 
+	extends Neurone
+	func fire(): super()
+class InputN: 
+	extends Neurone
+	func recieve(new_value): super(new_value)
+class MiddleN: 
+	extends Neurone
+	func fire(): super()
+	func recieve(new_value): super(new_value)
+
+func _to_string() -> String:
+	return self.name + " | " + str(self.value) + " | " + str(self.threshold) + " | " + str(self.recieved_value)
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept") && get_global_mouse_position().distance_to(self.position) < 5:
+		self.recieve(10)
+	if Input.is_action_just_pressed("ui_down") && get_global_mouse_position().distance_to(self.position) < 5:
+		print(self)
